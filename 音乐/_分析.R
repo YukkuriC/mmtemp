b358bd91795278571bed34adabe1b25a3691cfad
YukkuriC <34098703+YukkuriC@users.noreply.github.com>
2018-05-04 16:57:33 +0800

    Add files via upload

--- 音乐/_分析.R
+++ 音乐/_分析.R
@@ -13,33 +13,43 @@ for(song in c('luv letter','Flower Dance','克罗地亚狂想曲')){
   
   plot_seq=ggplot()+theme_bw()+theme(panel.grid.minor.y = element_blank())+
     labs(x='时间/拍',y='频率/Hz')+scale_color_hue('律制')+
-    geom_hline(aes(yintercept=440))+scale_y_log10(breaks=seq(200,1200,200))
-  for(i in 1:3){
-    plot_seq=plot_seq+eval(parse(
-      text=sprintf(
-        'geom_segment(data=fdata,aes(x=from,xend=to,y=%s,yend=%s,color="%s"),alpha=0.5)',
-        type[i],type[i],name[i]
-      )
-    ))
-  }
+    geom_hline(aes(yintercept=440))+scale_y_log10(breaks=seq(200,1200,200))+
+    geom_segment(data=fdata,aes(x=from,xend=to,y=equal,yend=equal,color="十二平均律"),alpha=0.5)+
+    geom_segment(data=fdata,aes(x=from,xend=to,y=pyth,yend=pyth,color="五度相生律"),alpha=0.5)+
+    geom_segment(data=fdata,aes(x=from,xend=to,y=pure,yend=pure,color="纯律"),alpha=0.5)
   
-  pdf(sprintf('%s_时间序列.pdf',song),10,8,family="GB1")
+  pdf(sprintf('%s_时间序列.pdf',song),7,6,family="GB1")
   print(plot_seq)
   graphics.off()
   
   # 分布图
-  plot_dist=ggplot()+theme_bw()+labs(x='音程长度/音分',y='密度')+scale_color_hue('律制')
-  for(i in 1:3){
-    plot_dist=plot_dist+eval(parse(
-      text=sprintf(
-        'stat_density(aes(x=log(fdata$%s[2:nrow(fdata)]/fdata$%s[1:(nrow(fdata)-1)],2)*1200,color="%s"),alpha=0.5,bw=6,geom="line")',
-        type[i],type[i],name[i]
-      )
-    ))
-  }
+  plot_dist=ggplot()+theme_bw()+labs(x='音程长度/音分',y='密度')+scale_color_hue('律制')+
+    stat_density(aes(x=log(fdata$equal[2:nrow(fdata)]/fdata$equal[1:(nrow(fdata)-1)],2)*1200,color="十二平均律"),alpha=0.5,bw=6,geom="line")+
+    stat_density(aes(x=log(fdata$pyth[2:nrow(fdata)]/fdata$pyth[1:(nrow(fdata)-1)],2)*1200,color="五度相生律"),alpha=0.5,bw=6,geom="line")+
+    stat_density(aes(x=log(fdata$pure[2:nrow(fdata)]/fdata$pure[1:(nrow(fdata)-1)],2)*1200,color="纯律"),alpha=0.5,bw=6,geom="line")
   
-  pdf(sprintf('%s_音程分布.pdf',song),10,8,family="GB1")
+  pdf(sprintf('%s_音程分布.pdf',song),7,6,family="GB1")
   print(plot_dist)
   graphics.off()
   
+  #音分差统计
+  data_count=rbind(
+    cbind(
+      as.data.frame(table(round(with(fdata,log(pyth/equal,2)*1200)))),
+      type='五度相生律'
+    ),
+    cbind(
+      as.data.frame(table(round(with(fdata,log(pure/equal,2)*1200)))),
+      type='纯律'
+    )
+  )
+  data_count$Var1=as.numeric(as.vector(data_count$Var1))
+  
+  plot_diff=ggplot(data=data_count)+theme_bw()+labs(x='与十二平均律相差/音分',y='频数')+scale_color_hue('律制')+
+    geom_segment(aes(x=Var1,xend=Var1,y=0,yend=Freq,color=type))+
+    geom_point(aes(x=Var1,y=Freq,color=type))
+  
+  pdf(sprintf('%s_音分差分布.pdf',song),7,6,family="GB1")
+  print(plot_diff)
+  graphics.off()
 }
